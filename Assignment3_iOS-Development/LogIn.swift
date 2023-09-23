bfc9f017739588a1774e3cd05164b724aab27e37
Tuan Nguyen <s4023955@rmit.edu.vn>
2023-09-23 13:53:20 +0700

    Update TextField for logIn and Registration Adding condition for email and password

--- Assignment3_iOS-Development/LogIn.swift
+++ Assignment3_iOS-Development/LogIn.swift
@@ -52,6 +52,8 @@ struct LogIn: View {
 
                         }
                         .background(Color(.systemCyan))
+                        .disabled(!formIsValid)
+                        .opacity(formIsValid ? 1.0 : 0.5)
                         .cornerRadius(10)
                         .padding(.top, 24)
                         
@@ -73,6 +75,16 @@ struct LogIn: View {
                 }
             }
         }
+    }
+}
+
+// AuthenticationFormProtocol, put condition 
+extension LogIn: AuthenticationFormProtocol {
+    var formIsValid: Bool {
+        return !email.isEmpty
+        && email.contains("@")
+        && !password.isEmpty
+        && password.count > 6
     }
 }
 
--- Assignment3_iOS-Development/Registration.swift
+++ Assignment3_iOS-Development/Registration.swift
@@ -23,12 +23,16 @@ struct Registration: View {
                 
                 InputView(text: $password, title: "Password", textField: "******", secureField: true)
                 
-                InputView(text: $confirmPassword, title: "Confirm Password", textField: "******", secureField: true)
+                ZStack(alignment: .trailing) {
+                    InputView(text: $confirmPassword, title: "Confirm Password", textField: "******", secureField: true)
+                    
+                    //adding checkup icon (if needed)
+                }
             }
             .padding(.horizontal)
             .padding(.top, 12)
             
-            
+            // Sign Up Button
             Button {
                 Task{
                     try await viewModel.createUser(withEmail: email,password: password)
@@ -43,6 +47,8 @@ struct Registration: View {
 
             }
             .background(Color(.systemCyan))
+            .disabled(!formIsValid)
+            .opacity(formIsValid ? 1.0 : 0.5)
             .cornerRadius(10)
             
             Spacer()
@@ -51,13 +57,24 @@ struct Registration: View {
                 dismiss()
             } label: {
                 HStack {
-                    Text("Sign In!!")
+                    Text("Back to Sign In!!")
                         .fontWeight(.bold)
                 }
                 .font(.system(size: 15))
             }
 
         }
+    }
+}
+
+extension Registration: AuthenticationFormProtocol {
+    var formIsValid: Bool {
+        return !email.isEmpty
+        && email.contains("@")
+        && !password.isEmpty
+        && password.count > 6
+        && !confirmPassword.isEmpty
+        && confirmPassword == password
     }
 }
 
